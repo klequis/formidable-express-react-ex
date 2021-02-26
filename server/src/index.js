5d4d196c7e3e8a584f779a1dfdb6ac5ca06c87cf
klequis <klequis@gmail.com>
2021-02-26 10:15:31 -0800

    before add formidable local

--- server/src/index.js
+++ server/src/index.js
@@ -1,73 +1,71 @@
-'use strict'
-
-import express from 'express'
+const { wrap } = require('module')
+import cors from 'cors'
 import formidable from 'formidable'
 import path from 'path'
-import cors from 'cors'
-import * as R from 'ramda'
 import fse from 'fs-extra'
-const app = express()
+
+const app = require('express')()
 app.use(cors())
 
-const moveFiles = async (uploadDir, files) => {
-  const all = Promise.all(
-    files.map(async (f) => {
-      return fse.move(f.path, path.join(uploadDir, f.name))
+app.get(
+  '*',
+  wrapAsync(async function (req, res) {
+    await new Promise((resolve) => setTimeout(() => resolve(), 50))
+    // Async error!
+    throw new Error('woops')
+  })
+)
+
+app.post(
+  '/api/upload',
+  wrapAsync(async function (req, res) {
+    const uploadDir = path.join(__dirname, '../uploads')
+    const form = formidable({
+      multiples: true
+      // uploadDir: uploadDir - doesn't work :(
     })
-  )
-  return all
+
+    form.parse(req, async (err, fields, files) => {
+      console.log('files', files)
+      if (err) {
+        next(err)
+        return
+      }
+      // const x = await moveFiles(uploadDir, files.uploadedFiles)
+
+      // -->
+      // try {
+      const a = await Promise.all(
+        files.uploadedFiles.map(async (f) => {
+          return fse.move(f.path, path.join(uploadDir, f.name))
+        })
+      )
+      // } catch (e) {
+      // console.log('** re-throwing')
+      // snext(e)
+      // }
+      // <--
+
+      res.json({ fields, files, uploadDir })
+    })
+    // -->
+    // await new Promise((resolve) => setTimeout(() => resolve(), 50))
+    // throw new Error('woops')
+    // <--
+  })
+)
+
+app.use(function (error, req, res, next) {
+  // Gets called because of `wrapAsync()`
+  res.json({ message: error.message })
+})
+
+app.listen(4040)
+
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    // Make sure to `.catch()` any errors and pass them along to the `next()`
+    // middleware in the chain, in this case the error handler.
+    fn(req, res, next).catch(next)
+  }
 }
-
-app.post('/api/upload', (req, res, next) => {
-  const uploadDir = path.join(__dirname, '../uploads')
-
-  const form = formidable({
-    multiples: true
-    // uploadDir: uploadDir - doesn't work :(
-  })
-
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      next(err)
-      return
-    }
-    // console.log('type', R.type(files.uploadedFiles))
-    // console.log('files.uploadFiles', files.uploadedFiles)
-
-    // files.uploadedFiles.map((f) => {
-    //   console.log({
-    //     path: f.path,
-    //     name: f.name,
-    //     type: f.type
-    //   })
-    // })
-
-    // currPath = f.path
-
-    // const moves = R.map(async (f) => {
-    //   console.log({
-    //     path: f.path,
-    //     name: f.name,
-    //     type: f.type
-    //   })
-    //   return
-    // }, files.uploadedFiles)
-    // const m = await Promise.all([moves])
-    const m = await moveFiles(uploadDir, files.uploadedFiles)
-    console.log('m', m)
-    // need to do fse.move with promise.all
-    // R.map(f => await fse.move(f.path, ))
-    // const movedFiles = promise.all([
-    //   fse.move()
-    // ])
-    res.json({ fields, files, uploadDir })
-  })
-})
-
-app.get('/api/test', async (req, res, next) => {
-  res.json({ result: 'success' })
-})
-
-app.listen(4040, () => {
-  console.log('Server listening on http://localhost:4040 ...')
-})
